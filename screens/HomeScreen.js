e576fce971a7861bc07be16e3c837f88fad74ed4
Brandon Vu <bvu106038@gmail.com>
2023-03-15 16:03:17 -0700

    add login homepage

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,18 +1,43 @@
-import React, { useState } from 'react';
-import { Button, Text, TextInput, StyleSheet, View} from 'react-native';
+import React, { useState } from "react";
+import { Button, Text, TextInput, StyleSheet, View } from "react-native";
+import { firebaseAuth, provider } from "../environment/config";
+import { getAuth, signOut } from "firebase/auth";
 
-const HomeScreen = ({navigation}) => {
-  const [searchValue, setSearchValue] = useState('');
-
+const HomeScreen = ({ navigation }) => {
+  const [searchValue, setSearchValue] = useState("");
+  const [statusLogin, setStatusLogin] = useState(false);
+  const [buttonLogin, setButtonLogin] = useState("Login");
   const handleSearch = () => {
     console.log(`Searching for ${searchValue}`);
     // Add your search logic here
   };
 
+  const buttonMode = () => {
+    setStatusLogin(!statusLogin);
+    if (statusLogin) {
+      console.log("log out mode");
+      const auth = getAuth();
+      signOut(auth)
+        .then(() => {
+          // Sign-out successful.
+        })
+        .catch((error) => {
+          // An error happened.
+        });
+        setButtonLogin("Login");
+    } else {
+      setButtonLogin("Logout");
+      console.log("log in mode");
+      navigation.navigate("Login");
+    }
+  };
   return (
     <View style={styles.container}>
+      <Button title={buttonLogin} onPress={buttonMode} />
       <Text style={styles.title}>Welcome to ZotFoods</Text>
-      <Text style={styles.subtitle}>Get personalized food recommendations based on your preferences.</Text>
+      <Text style={styles.subtitle}>
+        Get personalized food recommendations based on your preferences.
+      </Text>
       <TextInput
         style={styles.searchBar}
         placeholder="Search for cuisine"
@@ -20,12 +45,6 @@ const HomeScreen = ({navigation}) => {
         onSubmitEditing={handleSearch}
         value={searchValue}
       />
-      <Button
-        title="Go to Login Page" 
-        onPress={() => {
-            navigation.navigate("Login")
-        }}
-    />
     </View>
   );
 };
@@ -33,32 +52,32 @@ const HomeScreen = ({navigation}) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#fff',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
   },
   title: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 16,
   },
   subtitle: {
     fontSize: 16,
-    textAlign: 'center',
+    textAlign: "center",
     marginHorizontal: 32,
   },
   searchBar: {
     height: 40,
-    width: '80%',
-    borderColor: 'gray',
+    width: "80%",
+    borderColor: "gray",
     borderWidth: 1,
     paddingHorizontal: 10,
     marginTop: 20,
   },
 
   image: {
-    width: '100%',
-    height: '30%',
+    width: "100%",
+    height: "30%",
     marginTop: 20,
   },
 });
