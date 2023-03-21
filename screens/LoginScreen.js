9808e66a989bf08bfb20714988127e80b09cbac7
Brandon Vu <51305295+brandonvu12@users.noreply.github.com>
2023-03-20 21:26:12 -0700

    update bonk firebase

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,39 +1,71 @@
 import React, { useState } from "react";
-import { Button, StyleSheet, Text , View} from 'react-native';
-import { firebaseAuth, provider } from '../environment/config';
-import {signInWithPopup} from "firebase/auth";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { firebaseAuth, provider } from "../environment/config";
+import { signInWithPopup } from "firebase/auth";
+import { getDatabase, ref, set, onValue } from "firebase/database";
 
-export const RegistrationScreen = ({navigation}) => {
-  function googleLogin() {
-    signInWithPopup(firebaseAuth, provider).then((data) => {
-      const userData = data.user
-      console.log(userData.displayName)
-      console.log(userData.email)
-    }).then(() => navigation.navigate("Home"))
-
+export const RegistrationScreen = ({ navigation }) => {
+  function readData() {
+    const db = getDatabase();
+    const tasksRef = ref(db, "Users/");
+    onValue(tasksRef, (snapshot) => {
+      const data = snapshot.val();
+      console.log("data", data);
+      // updateStarCount(postElement, data);
+    });
+    // return 0;
   }
+  function writeUserData(userId, name, email) {
+    const db = getDatabase();
+    const tasksRef = ref(db, "Users/");
+    // var playersRef = firebase.database().ref("Users/");
+    set(tasksRef, {
+      UserName: name,
+      UserEmail: email,
+    });
+    console.log("Sent");
+  }
+  function googleLogin() {
+    console.log("about to read")
+    readData()
+    console.log("read it")
+    signInWithPopup(firebaseAuth, provider)
+      .then((data) => {
+        const userData = data.user;
+        console.log(userData.uid);
+        console.log(userData.displayName);
+        console.log(userData.email);
+        writeUserData(userData.uid, userData.displayName, userData.email);
+      })
+      .then(() => navigation.navigate("Home"));
+    
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Sign in to your Account with Google</Text>
-      <Button title={"Sign In With Google"} onPress={() => {
-        googleLogin()
-      }} />
+      <Text style={styles.title}>Login to your Account with Google</Text>
+      <Button
+        title={"Sign In With Google"}
+        onPress={() => {
+          googleLogin();
+        }}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: "center",
-      justifyContent: "center",
-      backgroundColor: "#fff",
-    },
-    title: {
-      fontSize: 24,
-      fontWeight: "bold",
-      marginBottom: 16,
-    },
-  });
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 16,
+  },
+});
 
 export default RegistrationScreen;
