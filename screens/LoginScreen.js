5c5d5e1cf9dac6d6bebfa9b509c8b43c4a0289ec
Brandon Vu <bvu106038@gmail.com>
2023-03-20 18:34:24 -0700

    print database

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -2,16 +2,36 @@ import React, { useState } from "react";
 import { Button, StyleSheet, Text , View} from 'react-native';
 import { firebaseAuth, provider } from '../environment/config';
 import {signInWithPopup} from "firebase/auth";
+import { getDatabase, ref, set } from "firebase/database";
+// import { getFirestore } from "firebase/firestore";
+import firestore from '@react-native-firebase/firestore';
+
+
+
 
 export const RegistrationScreen = ({navigation}) => {
   function googleLogin() {
+    const usersCollection = firestore().collection('Users');
+    console.log(usersCollection)
     signInWithPopup(firebaseAuth, provider).then((data) => {
       const userData = data.user
+      console.log(userData.uid)
       console.log(userData.displayName)
       console.log(userData.email)
+      writeUserData(userData.uid, userData.displayName, userData.email)
     }).then(() => navigation.navigate("Home"))
 
   }
+
+  function writeUserData(userId, name, email) {
+    const db = getDatabase();
+    set(ref(db, 'users/' + userId), {
+      username: name,
+      email: email,
+    });
+  }
+
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Login to your Account with Google</Text>
